3f5f575a394be85317c49deab0f65da8c26d7a77
PacharaWin <pacharawinboon@gmail.com>
2023-11-11 23:52:34 +0700

    fix user

--- src/logic/user.js
+++ src/logic/user.js
@@ -1,3 +1,8 @@
+import { API_GATEWAY_URL } from "@/variable";
+
+const AUTH_URL = `${API_GATEWAY_URL}/auth`
+const USER_URL = `${API_GATEWAY_URL}/users`
+
 const getUserId = (router) => {
     const user = sessionStorage.getItem('user')
     if (user){
@@ -9,6 +14,25 @@ const getUserId = (router) => {
     }
 }
 
+const getUserById = async (userId) => {
+    try{
+        const respone = await fetch(`${USER_URL}/${userId}`, {
+            method: "GET",
+        });
+        const result = await respone.json();
+        if (!respone.ok) {
+            return {err:true, result: null};
+        } else {
+            return {err:false, result};
+        }
+
+    } catch (error) {
+        console.log(error)
+        return {err:true, result: null};
+    }
+    
+}
 module.exports = {
-    getUserId
+    getUserId,
+    getUserById
 }
