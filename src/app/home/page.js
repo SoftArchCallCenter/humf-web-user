7003e2443ba9452842fe49098511b61c1b19b865
PacharaWin <pacharawinboon@gmail.com>
2023-11-12 00:19:31 +0700

    fix page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -46,21 +46,15 @@ export default function Home() {
 			return (
 				<div className="mt-10 flex items-center justify-center">
 					<div className="w-80 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-						<h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{restaurant.name}</h5>
-						<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Address : {restaurant.address}</p>
-						<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Open Time : {restaurant.openTime}</p>
-						<p className="mb-4 font-normal text-gray-700 dark:text-gray-400">Close Time : {restaurant.closeTime}</p>
-						<a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => removeRestaurant(restaurant.id)}>
-							<svg className="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-								<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12M1 5"/>
-							</svg>
-							Remove restaurant
-						</a>
+						<h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{user.name}</h5>
+						<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Address : {user.email}</p>
+						{/* Do profile pic too */}
 					</div>
 				</div>
 			)
 		} else {
 			return (
+				// For no user?
 				<div className="mt-10 flex items-center justify-center">
 					<div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 						<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">There is no restaurant</h5>
